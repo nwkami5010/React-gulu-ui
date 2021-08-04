c197e77ed5cce9ae4fc3635050955e5295266ea7
nwkami <715190184@qq.com>
2021-08-04 22:36:32 +0800

    webpack-dev-server 自动打包

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
-        library: 'FUI',
+        library: 'React-gulu-ui',
         libraryTarget: 'umd',
     },
     module: {
