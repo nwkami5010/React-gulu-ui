8ee53dc236702e0c9c81d2b4d28a4616814fda9e
nwkami <715190184@qq.com>
2021-08-06 12:44:45 +0800

    直接copy最终版的package.json，index.d.ts 移到 dist 目录里了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Button from './button';
-console.log(React)
-console.log(ReactDOM)
-ReactDOM.render(<Button></Button>,document.body)
+
+
+
+
+const a = 1
+
+
+
+
+export {Button, a};
