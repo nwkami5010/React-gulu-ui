aa4a694787c8a0c20f23de55b39c24f86f5b2b06
nwkami <715190184@qq.com>
2021-08-09 16:43:04 +0800

    简化参数

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './importIcons';
 import './icon.scss';
-interface  IconProps {
+import classes from './helpers/classes';
+
+interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
-  onClick: React.MouseEventHandler<SVGElement>
 }
 
-const Icon: React.FC<IconProps> = (props) => {
-  return (
-    <svg className="hu-icon" onClick={props.onClick}>
-      <use xlinkHref={`#${props.name}`}/>
-    </svg>
-  );
+const Icon: React.FunctionComponent<IconProps> =
+  ({className, name, ...restProps}) => {
+    return (
+      <svg className={classes('fui-icon', className)}
+           {...restProps}
+      >
+        <use xlinkHref={`#${name}`}/>
+      </svg>
+    );
 };
 export default Icon;
